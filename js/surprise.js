4fa9b52c32e57af547c05c519f06bd6d1472cde3
Ellie Hollenberger <46726245+elliehollenberger@users.noreply.github.com>
2020-03-22 16:00:51 -0500

    Add files via upload

---
+++ js/surprise.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  $(".camo2").click(function() {
+    $(".omg").toggleClass("omg2");
+    $(".text2").toggleClass("text22");
+    $(".roomates").toggleClass("roomates2");
+    $(".bg").toggleClass("bg2");
+    $(".camo").fadeOut("fast");
+    $(".camo2").fadeOut("fast");
+    $(".text1").fadeOut("fast");
+  });
+
+
+});
